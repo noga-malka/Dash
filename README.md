c5d4a5c1cda1041deaa4de03750a0635d508cafe
Motti Weber <MottiW@cthil.com>
2022-10-20 14:38:57 +0300

    get serial or bluetooth from command line

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Dash
+# Setup
+
+```commandline
+pip install -r requirements.txt
+```
+
+# Rum Program
+
+## data from serial port:
+
+```
+py ./app.py serial
+```
+
+## data from bluetooth:
+
+```
+py ./app.py bluetooth
+```
--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import sys
 from threading import Thread
 
 from callbacks import dash_app
 from handlers.bluethooth_reader import BluetoothHandler
+from handlers.serial_reader import SerialHandler
 from layout import make_layout
 from utilities import save_serial_data
 
 if __name__ == '__main__':
-    # handler = SerialHandler()
-    handler = BluetoothHandler()
-    Thread(target=save_serial_data, args=[handler]).start()
-    dash_app.layout = make_layout()
-    dash_app.run_server(debug=True, use_reloader=False)
+    types = {'serial': SerialHandler, 'bluetooth': BluetoothHandler}
+    if len(sys.argv) == 1 or sys.argv[1] not in types:
+        print('invalid handler type received')
+    else:
+        handler = types.get(sys.argv[1])()
+        Thread(target=save_serial_data, args=[handler]).start()
+        dash_app.layout = make_layout()
+        dash_app.run_server(debug=True, use_reloader=False)
