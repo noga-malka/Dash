46938f87bf785124a9ce282d9f59b3821a723f3d
Motti Weber <MottiW@cthil.com>
2022-11-11 20:44:51 +0200

    Add cleanup at the start of a stoppable thread

--- stoppable_thread.py
+++ stoppable_thread.py
@@ -5,7 +5,7 @@ class StoppableThread(Thread):
     def __init__(self, target=None, name=None, args=(), kwargs=None, daemon=None, cleanup=None):
         super(StoppableThread, self).__init__(target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
         self._stop_event = Event()
-        self.cleanup = cleanup
+        self.cleanup = cleanup if cleanup else lambda: None
 
     def stop(self):
         self._stop_event.set()
@@ -14,7 +14,7 @@ class StoppableThread(Thread):
         return self._stop_event.is_set()
 
     def run(self) -> None:
+        self.cleanup()
         while not self.is_stopped():
             self._target(*self._args, **self._kwargs)
-        if self.cleanup:
-            self.cleanup()
+        self.cleanup()
