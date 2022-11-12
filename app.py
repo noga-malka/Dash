456053d37fc1502a5dedb82123fa8ecf528b86f2
Motti Weber <MottiW@cthil.com>
2022-11-12 15:12:07 +0200

    Remove sys.args use

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import sys
-
 from callbacks import app
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('invalid handler type received')
-    else:
-        app.run_server(debug=True)
-        # WebUI(dash_app).run()
+    app.run_server(debug=True)
+    # WebUI(dash_app).run()
