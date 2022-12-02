c07186f3b31db104141a73c801a24e9524ed69c3
Motti Weber <MottiW@cthil.com>
2022-12-02 22:43:28 +0200

    Open in webui in production mode

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import sys
+
+from webui import WebUI
+
 from callbacks import app
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # WebUI(dash_app).run()
+    is_debug = len(sys.argv) > 1 and sys.argv[1] == 'debug'
+    if is_debug:
+        app.run_server(debug=True)
+    else:
+        WebUI(app).run()
