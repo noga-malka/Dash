f2a63433a6b0f762d2c6daee6abf42677b67e32e
Motti Weber <MottiW@cthil.com>
2022-12-03 18:33:48 +0200

    Use events in stoppable_thread.py

--- stoppable_thread.py
+++ stoppable_thread.py
@@ -1,23 +1,56 @@
+import time
 from threading import Thread, Event
+
+from handlers.bluethooth_reader import BluetoothHandler
+from handlers.file_handler import FileHandler
+from handlers.random_handler import RandomHandler
+from handlers.serial_reader import SerialHandler
+
+types = {'serial': SerialHandler(), 'bluetooth': BluetoothHandler(), 'random': RandomHandler(), 'upload': FileHandler()}
+
+
+class Events:
+    change_input = Event()
+    connect = Event()
+    clean = Event()
+
+    class Finish:
+        connect = Event()
+        clean = Event()
 
 
 class StoppableThread(Thread):
-    def __init__(self, target=None, name=None, args=(), kwargs=None, daemon=None, cleanup=None, setup=None):
+    def __init__(self, target=None, name=None, args=(), kwargs=None, daemon=None):
         super(StoppableThread, self).__init__(target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
-        self._stop_event = Event()
-        self.cleanup = cleanup if cleanup else lambda: None
-        self.setup = setup
+        self.events = Events()
+        self.handler_name = ''
 
-    def stop(self):
-        self._stop_event.set()
+    def set_handler(self, handler_name):
+        self.handler_name = handler_name if handler_name in types else ''
+        self.events.change_input.set()
 
-    def is_stopped(self):
-        return self._stop_event.is_set()
+    def connect_handler(self, **kwargs):
+        if self.handler_name:
+            is_connected = types[self.handler_name].connect(**kwargs)
+            if is_connected:
+                self.events.Finish.connect.set()
+
+    def cleanup(self):
+        if not self.events.clean.is_set():
+            self.events.clean.set()
+        self._target(*self._args, **self._kwargs)
+        self.events.Finish.clean.wait()
+        self.events.clean.clear()
+        self.events.Finish.clean.clear()
 
     def run(self) -> None:
-        self.cleanup()
-        if self.setup:
-            self._target = self.setup(*self._args, **self._kwargs)
-        while not self.is_stopped():
-            self._target(*self._args, **self._kwargs)
-        self.cleanup()
+        while True:
+            time.sleep(0.001)
+            if self.events.change_input.is_set():
+                self.cleanup()
+                self.events.change_input.clear()
+                self.events.Finish.connect.wait()
+            if self.events.clean.is_set():
+                self.cleanup()
+            elif self.events.Finish.connect.is_set():
+                self._target(*self._args, **self._kwargs)
