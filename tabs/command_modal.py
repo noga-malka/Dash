d681ab436981cb809e86bffa4dec75d018418063
Motti Weber <MottiW@cthil.com>
2022-12-03 18:35:20 +0200

    Build modal in a generic function

--- tabs/command_modal.py
+++ tabs/command_modal.py
@@ -5,19 +5,27 @@ from consts import Commands
 
 
 def command_modal():
+    inputs = [
+        dcc.Dropdown(Commands.ALL, id='command_menu', value=Commands.ALL[0]),
+        dbc.Input(id='command_input', value=Commands.COMMAND_DEFAULT.get(Commands.ALL[0]),
+                  style={'width': 'auto'}),
+
+    ]
+    return modal_generator('modal', 'Command Sender', 'send_command', 'Send Command', inputs)
+
+
+def modal_generator(modal_id: str, title: str, button_id: str, button_text: str, inputs: list, is_open=False):
     return html.Div(
         [
             dbc.Modal(
                 [
-                    dbc.ModalHeader(html.H3("Command Sender")),
+                    dbc.ModalHeader(html.H3(title)),
                     dbc.ModalBody([
-                        dcc.Dropdown(Commands.ALL, id='command_menu', value=Commands.ALL[0]),
-                        dbc.Input(id='command_input', value=Commands.COMMAND_DEFAULT.get(Commands.ALL[0]),
-                                  style={'width': 'auto'}),
-                        dbc.Button('Send Command', id='send_command'),
+                        *inputs,
+                        dbc.Button(button_text, id=button_id),
                     ], className='center align children-margin'),
                 ],
-                id="modal", centered=True
+                id=modal_id, centered=True, is_open=is_open
             ),
         ]
     )
