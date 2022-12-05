d7f5568cac0a6621049d3295557ed42bd488bfed
Motti Weber <MottiW@cthil.com>
2022-12-05 16:40:15 +0200

    Remove use of low and high in slider

--- monitors/slider_monitor.py
+++ monitors/slider_monitor.py
@@ -9,7 +9,6 @@ class SliderMonitor(Monitor):
     def generate_daq(self, monitor_id):
         label = html.Label(self.sensor.label, className='label-style')
         slider = daq.Slider(id=monitor_id, handleLabel=self.sensor.label,
-                            value=self.sensor.minimum,
-                            targets={str(self.sensor.low): 'low', str(self.sensor.high): 'high'},
+                            value=self.sensor.minimum, targets={},
                             max=self.sensor.maximum, min=self.sensor.minimum, size=self.size, disabled=True)
         return [label, slider, self.generate_led(monitor_id)]
