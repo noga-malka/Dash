02286cd3bff32f27678f75052591910a29bfaf51
Motti Weber <MottiW@cthil.com>
2022-12-05 12:05:36 +0200

    Remove webui use

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from webui import WebUI
-
 from callbacks import app
-from consts import IS_DEBUG
 
 if __name__ == '__main__':
-    if IS_DEBUG:
-        app.run_server(debug=True)
-    else:
-        WebUI(app).run()
+    app.run_server(debug=True)
