4c641325e80a9ba580af59b90e3e01091e096c64
Motti Weber <MottiW@cthil.com>
2022-12-05 16:41:25 +0200

    Choose colors according to ValueRange.LEVEL_COMPARE

--- monitors/basic_monitor.py
+++ monitors/basic_monitor.py
@@ -1,7 +1,7 @@
 import dash_daq as daq
 from dash import html, Output
 
-from consts import TagIds
+from consts import TagIds, Colors, ValueRange
 
 
 class Monitor:
@@ -19,14 +19,20 @@ class Monitor:
         return [Output(sensor_key, 'value'), Output(sensor_key, 'color'), Output(sensor_key + '_led', 'value'),
                 Output(sensor_key + '_led', 'color'), Output(sensor_key + '_icon', 'className')]
 
-    def generate_output_values(self, value):
-        is_valid = self.sensor.low < value < self.sensor.high
-        icon = f'{TagIds.Icons.CHECK} valid' if is_valid else f'{TagIds.Icons.WARNING} invalid'
-        color = '#ABE2FB' if is_valid else 'red'
-        return [value, color, value, color, f'fa {icon}']
+    @staticmethod
+    def _get_icon(is_valid):
+        return 'fa ' + (f'{TagIds.Icons.CHECK} valid' if is_valid else f'{TagIds.Icons.WARNING} invalid')
 
-    def generate_led(self, led_id):
-        icon = html.Div(id=led_id + '_icon', className=f'fa {TagIds.Icons.WARNING} invalid')
+    def generate_output_values(self, value):
+        sensor_dict = self.sensor.dict()
+        current_level = Colors.ERROR
+        for (minimum, maximum), level in ValueRange.LEVEL_COMPARE.items():
+            if sensor_dict[minimum] <= value <= sensor_dict[maximum]:
+                current_level = level
+        return [value, current_level, value, current_level, self._get_icon(current_level == Colors.GOOD)]
+
+    def generate_led(self, led_id, color=Colors.ERROR, is_valid=False):
+        icon = html.Div(id=led_id + '_icon', className=self._get_icon(is_valid))
         return html.Div(
-            [icon, daq.LEDDisplay(id=led_id + '_led', value=self.sensor.minimum, size=20, color='red')],
+            [icon, daq.LEDDisplay(id=led_id + '_led', value=self.sensor.minimum, size=20, color=color)],
             className='center align children-margin-2')
