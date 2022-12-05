95ce411ed1c64eed8d946cb2473247b87285de08
Motti Weber <MottiW@cthil.com>
2022-12-05 14:02:54 +0200

    Add sliderMonitor

---
+++ monitors/slider_monitor.py
@@ -0,0 +1,15 @@
+import dash_daq as daq
+from dash import html
+
+from monitors.basic_monitor import Monitor
+
+
+class SliderMonitor(Monitor):
+
+    def generate_daq(self, monitor_id):
+        label = html.Label(self.sensor.label, className='label-style')
+        slider = daq.Slider(id=monitor_id, handleLabel=self.sensor.label,
+                            value=self.sensor.minimum,
+                            targets={str(self.sensor.low): 'low', str(self.sensor.high): 'high'},
+                            max=self.sensor.maximum, min=self.sensor.minimum, size=self.size, disabled=True)
+        return [label, slider, self.generate_led(monitor_id)]
