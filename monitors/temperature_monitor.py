f0857d713a58616c36962718fe8d484142fdcdc4
Motti Weber <MottiW@cthil.com>
2022-12-14 18:26:40 +0200

    Parse minimum and maximum temperature by unit in monitor creation

--- monitors/temperature_monitor.py
+++ monitors/temperature_monitor.py
@@ -1,13 +1,15 @@
 import dash_daq as daq
 
+from consts import UnitTypes
 from monitors.basic_monitor import Monitor
 
 
 class TemperatureMonitor(Monitor):
 
     def generate_daq(self, monitor_id):
+        change_function = UnitTypes.CONVERT[self.sensor.unit_type]
         temp = daq.Thermometer(id=monitor_id, label=self.sensor.label, value=self.sensor.minimum,
                                showCurrentValue=True, units=self.sensor.unit_type,
-                               min=self.sensor.minimum, max=self.sensor.maximum,
+                               min=change_function(self.sensor.minimum), max=change_function(self.sensor.maximum),
                                height=self.size, width=self.size / 10)
         return [temp, self.generate_led(monitor_id)]
