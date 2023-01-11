0810a126971534cf1890299974015fb9363e0968
Motti Weber <MottiW@cthil.com>
2023-01-11 18:31:07 +0200

    Import all callback files

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from callbacks import app
+from default import app
+from callbacks.configuration_page import *
+from callbacks.general import *
+from callbacks.live_page import *
+from callbacks.graph_page import *
 from consts import IS_DEBUG
 
 if __name__ == '__main__':
