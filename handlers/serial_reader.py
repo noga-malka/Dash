580cd5f33a9c34695b14a1d9edbdff0537605e24
Motti Weber <MottiW@cthil.com>
2023-02-07 14:04:44 +0200

    Move serial discover function to multiple serial handler

--- handlers/serial_reader.py
+++ handlers/serial_reader.py
@@ -9,11 +9,7 @@ from handlers.handler_exception import DisconnectionEvent
 
 class SerialHandler(Handler):
     def __init__(self):
-        self.devices = []
         super(SerialHandler, self).__init__(False)
-
-    def discover(self):
-        self.devices = [com[0] for com in filter(lambda port: "USB" in port[2], list_ports.comports())]
 
     def connect(self, comport=None, **kwargs):
         self.disconnect()
