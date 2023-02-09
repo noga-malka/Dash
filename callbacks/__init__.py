09c4be26944518f6a78a18db208766d2918d7434
Motti Weber <MottiW@cthil.com>
2023-02-09 16:43:10 +0200

    Import all callbacks in __init__.py

---
+++ callbacks/__init__.py
@@ -0,0 +1,7 @@
+from callbacks.configuration_table import *
+from callbacks.ds_sensor_modal import *
+from callbacks.general import *
+from callbacks.graph_page import *
+from callbacks.live_page import *
+from callbacks.modals import *
+from callbacks.placeholder import *
