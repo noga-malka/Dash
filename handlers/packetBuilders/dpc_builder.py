9129d91c061a873b53df5787426aa37e71174129
Motti Weber <MottiW@cthil.com>
2023-04-17 22:37:04 +0300

    Delete dpc builder

--- handlers/packetBuilders/dpc_builder.py
+++
@@ -1,6 +0,0 @@
-from handlers.packetBuilders.builder import PacketBuilder
-
-
-class DPCPacketBuilder(PacketBuilder):
-    def build_packet(self, command: str, content: str = ''):
-        return bytes(command + content + '\r', 'utf-8')
